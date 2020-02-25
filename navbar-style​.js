a568a3f636db7ce0f9f7975501dfe1717425dc7a
aoaaceai <iamjames910118@gmail.com>
2020-02-25 21:28:07 +0800

    fixed 404 bug

--- navbar-style​.js
+++ navbar-style​.js
@@ -1,13 +1,6 @@
 var navbar=document.getElementsByTagName("nav")[0];
 var icon_size=navbar.getBoundingClientRect()["height"];
 window.addEventListener("hashchange", function() { scrollBy(0, -icon_size) });
-// var right_stuff=navbar.getElementsByClassName("content-change")
-// var i;
-
-// for(i=0;i<right_stuff.length;i++){
-//   right_stuff[i].getElementsByClassName("A")[0].style.height=(0.8*icon_size).toString()+"px";
-// }
-
 function redi(){
     window.location.replace("YW9udGhlcj8=.html");
 }
