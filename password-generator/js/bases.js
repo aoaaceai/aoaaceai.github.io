c61d4b7774498e55fd0d976b236a1a42ac46e2e5
aoaaceai <54458354+aoaaceai@users.noreply.github.com>
2020-10-31 10:37:34 +0800

    generator update

---
+++ password-generator/js/bases.js
@@ -0,0 +1,9 @@
+var numbers = "0123456789";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function baseConvert(number, ...using) {
+    using = Array.from(using);
+    console.log(using);
+    console.log(using.includes("hi"));
+}
